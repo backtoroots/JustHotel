42ae2071ed4fe3217c30696a71c6e3fdc644de0a
backtoyourroots <c.tchulaev@yandex.ru>
2020-09-08 23:13:56 +0300

    Initial commit

---
+++ app/src/main/java/com/example/justhotel/ui/home/HomeFragment.kt
@@ -0,0 +1,37 @@
+package com.example.justhotel.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.justhotel.R
+
+
+class HomeFragment : Fragment(), View.OnClickListener {
+
+    lateinit var navController: NavController
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val textView: TextView = root.findViewById(R.id.text_home)
+        return root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+    }
+
+    override fun onClick(v: View?) {
+        print(1)
+    }
+}
